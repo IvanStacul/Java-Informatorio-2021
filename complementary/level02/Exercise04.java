2f86d4dc0e17b170aaa58f351b49b089d1006723
IvanStacul <stacul.ivan98@gmail.com>
2021-12-17 22:41:38 -0300

    fix bug

--- complementary/level02/Exercise04.java
+++ complementary/level02/Exercise04.java
@@ -12,13 +12,16 @@ public class Exercise04 {
     public static void main(String[] args) {
         List<String> students = new ArrayList<>();
 
-        students.addAll(Arrays.asList("Ivan", "Angel", "Emilio", "Elias", "Lucas", "Matias", "Facundo", "Nicolas",
-                "Jose", "Maximiliano", "Braian", "Santiago"));
+        students.addAll(Arrays.asList(
+            "Ivan", "Agostina", "Matias", "Florencia", 
+            "Lucas", "Iara", "Facundo", "Nicolas",
+            "Juana", "Daiana", "Milena", "Santiago"
+        ));
 
         List<List<String>> courses = new ArrayList<List<String>>();
 
         for (int i = 0; i < 3; i++) {
-            courses.add(students.subList(0 + 4 * i, 3 + 4 * i));
+            courses.add(students.subList(0 + 4 * i, 3 + (4 * i + 1)));
         }
 
         System.out.println(courses);
